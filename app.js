80eeb36d124204519bc1d723a6fa0ae82ff8383d
dlycliu <dlycliu@cn.ibm.com>
2016-11-19 14:24:53 +0800

    fixed

--- app.js
+++ app.js
@@ -14,31 +14,30 @@ var cfenv = require("cfenv");
 
 // create a new express server
 var app = express();
-	
+
 //add http&socket module
-var http = require("http").Server(app);	
-var io = require("socket.io")(http);	
-	
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + "/public"));
 
 
-io.on("connection",function(socket){
-	
-	socket.on("String",function(key){	
-		if(key == "w" || key == "s" || key == "a" || key == "d"){	
-			console.log("Received " + key + " from frontend");
-			
-			//************************
-			//fill in a method "io.sockets.emit(param1,param2)" in line 35
-			//The first parameter is the data type "message", the second parameter is the data you want to send
-					
-		}
-		else{
-			console.log("Not a direction order");
-		}	
-	});
-			
+io.on("connection", function(socket) {
+
+    socket.on("String", function(key) {
+        if (key == "w" || key == "s" || key == "a" || key == "d") {
+            console.log("Received " + key + " from frontend");
+
+            //************************
+            //fill in a method "io.sockets.emit(param1,param2)" in line 35
+            //The first parameter is the data type "message", the second parameter is the data you want to send
+            io.sockets.emit('message', key);
+        } else {
+            console.log("Not a direction order");
+        }
+    });
+
 });
 
 // get the app environment from Cloud Foundry
@@ -47,6 +46,6 @@ var appEnv = cfenv.getAppEnv();
 // start server on the specified port and binding host
 
 http.listen(appEnv.port, "0.0.0.0", function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url + " " + appEnv.port);
+    // print a message when the server starts listening
+    console.log("server starting on " + appEnv.url + " " + appEnv.port);
 });
